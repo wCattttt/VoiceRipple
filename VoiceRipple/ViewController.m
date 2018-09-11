8a7fa3a4a1c421188f84554766ce073541577d4f
weiweilong <weiweilong@MacBook-Pro.local>
2018-09-11 08:40:12 +0800

    change

--- VoiceRipple/ViewController.m
+++ VoiceRipple/ViewController.m
@@ -34,22 +34,8 @@
 }
 
 - (void)_initVoice {
-    NSMutableDictionary *recordSetting = [[NSMutableDictionary alloc]init];
-    
-    //设置录音格式
-    [recordSetting setValue:[NSNumber numberWithInt:kAudioFormatLinearPCM ] forKey:AVFormatIDKey];
-    
-    //设置录音采样率(Hz) 如：AVSampleRateKey==8000/44100/96000（影响音频的质量）
-    [recordSetting setValue:[NSNumber numberWithFloat:8000] forKey:AVSampleRateKey];
-    
-    //录音通道数  1 或 2
-    [recordSetting setValue:[NSNumber numberWithInt:1] forKey:AVNumberOfChannelsKey];
-    
-    //线性采样位数  8、16、24、32
-    [recordSetting setValue:[NSNumber numberWithInt:16] forKey:AVLinearPCMBitDepthKey];
-    
-    //录音的质量
-    [recordSetting setValue:[NSNumber numberWithInt:AVAudioQualityHigh] forKey:AVEncoderAudioQualityKey];
+    // 初始化录音参数
+    NSDictionary *recordSetting = [self audioParamSetting];
     
     NSString *strUrl = [NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES) lastObject];
     NSURL *url = [NSURL fileURLWithPath:[NSString stringWithFormat:@"%@/Video.pcm", strUrl]];
@@ -63,16 +49,30 @@
 
     audioRecorder.delegate = self;
     
+    // 设备开启录音模式
+    AVAudioSession *audioSession = [AVAudioSession sharedInstance];
+    [audioSession setCategory:AVAudioSessionCategoryPlayAndRecord error:nil];
+    [audioSession setActive:YES error:nil];
+    
     //创建录音文件，准备录音
     BOOL prepareSuccess = [audioRecorder prepareToRecord];
     NSLog(@"准备录音: %d", prepareSuccess);
+}
+
+- (NSDictionary *)audioParamSetting {
+    NSMutableDictionary *recordSetting = [[NSMutableDictionary alloc]init];
+    //设置录音格式
+    [recordSetting setValue:[NSNumber numberWithInt:kAudioFormatLinearPCM ] forKey:AVFormatIDKey];
+    //设置录音采样率(Hz) 如：AVSampleRateKey==8000/44100/96000（影响音频的质量）
+    [recordSetting setValue:[NSNumber numberWithFloat:8000] forKey:AVSampleRateKey];
+    //录音通道数  1 或 2
+    [recordSetting setValue:[NSNumber numberWithInt:1] forKey:AVNumberOfChannelsKey];
+    //线性采样位数  8、16、24、32
+    [recordSetting setValue:[NSNumber numberWithInt:16] forKey:AVLinearPCMBitDepthKey];
+    //录音的质量
+    [recordSetting setValue:[NSNumber numberWithInt:AVAudioQualityHigh] forKey:AVEncoderAudioQualityKey];
     
-    if(prepareSuccess){
-        BOOL seccess = [audioRecorder record];
-        NSLog(@"开始录音: %d", seccess);
-        [_rippleView startAnimation];
-    }
-    
+    return recordSetting;
 }
 
 #pragma mark 获取音量值
@@ -81,13 +81,11 @@
     
     //获取音量的平均值  [recorder averagePowerForChannel:0];
     //音量的最大值  [recorder peakPowerForChannel:0];
-    
     double lowPassResults = pow(10, (0.1 * [audioRecorder peakPowerForChannel:0]));
+    NSLog(@"%f", lowPassResults);
     
     _valueLabel.text = [NSString stringWithFormat:@"%f", lowPassResults];
     _rippleView.kRippleYMax = lowPassResults*30;
-    
-    return;
 }
 
 #pragma mark 开始/停止录音
